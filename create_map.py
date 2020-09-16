46947c45ad05c436c2d63e54b77e830a33668e61
npulver <nathan.pulver@jpl.nasa.gov>
2020-09-16 15:20:53 -0700

    added better functionality to interactive notebook and optimized map interface for less lag

--- create_map.py
+++ create_map.py
@@ -7,8 +7,22 @@ Created on Thu Jul 30 16:20:06 2020
 import folium
 from folium import plugins
 import branca.colormap as cm
+import matplotlib.pyplot as plt
 
 ucerf3 = 'UCERF3_.geojson'
+
+def create_contour_overlay(Lon,Lat,Z):
+    '''
+    input pandas df columns with 
+    '''
+    fig, ax = plt.subplots(nrows=1, ncols=1)
+    ax.tricontourf(Lon, Lat, Z, cmap='seismic')
+    fig.frameon  = False
+    ax.set_axis_off()
+    plt.close(fig)
+    fig.savefig(f'contour_of_{Z.name}.png')
+    
+    
 def create_map(df, lon_col='Lon', lat_col = 'Lat', data_col= 'Delta N'):
     '''
     creates folium map for data exploration using a pandas data frame.
@@ -20,7 +34,7 @@ def create_map(df, lon_col='Lon', lat_col = 'Lat', data_col= 'Delta N'):
                 df[lon_col].max())/2)
     my_map = folium.Map(start_cords,
                         tiles = 'Stamen Terrain',
-                        zoom_start = 8, width = 800, height = 350,
+                        zoom_start = 8, width = 600, height = 500,
                        prefer_canvas =True)
     minimap = plugins.MiniMap(toggle_display=True)
     my_map.add_child(minimap)
@@ -28,22 +42,16 @@ def create_map(df, lon_col='Lon', lat_col = 'Lat', data_col= 'Delta N'):
     folium.GeoJson(ucerf3, name = 'UCERF3 Faults').add_to(my_map)
     
 
-    colors_list = ['red', 'orange', 'yellow', 'green', 'blue', 'purple']
+    colors_list = ['blue', 'white', 'red']
     minimum = df[data_col].min()
     maximum = df[data_col].max()
     colormap = cm.LinearColormap(colors = colors_list
                         ,vmin = minimum
                         ,vmax= maximum)
-    data_group = folium.FeatureGroup(name = 'Velocities in mm/yr for ' + data_col)
-    df.apply(lambda row: folium.Circle(
-        location = (row[lat_col], row[lon_col]),
-        radius = 0.1,
-        fill = True, 
-        color= colormap(row[data_col]),
-        fill_opacity = 0.8
-        ).add_to(data_group), axis=1)          
+    create_contour_overlay(df[lon_col], df[lat_col], df[data_col])
+    
+    folium.raster_layers.ImageOverlay(f'contour_of_{df[data_col].name}.png', [[df.Lat.min(), df.Lon.min()],[df.Lat.max(), df.Lon.max() ]], opacity = 0.8, name = f'Velocity in mm/yr for {df[data_col].name}').add_to(my_map)
     my_map.add_child(colormap)
-    my_map.add_child(data_group)
     my_map.add_child(folium.LayerControl(postion='bottomleft'))
     my_map.add_child(folium.LatLngPopup())   
     return my_map
